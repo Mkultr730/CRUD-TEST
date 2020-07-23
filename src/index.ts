15ab0f642aa0777d4c560318261be929be4b6e92
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-23 00:39:38 -0500

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,20 @@ import { usersRouter } from './routes/usuarios'
 import { areasRouter } from './routes/areas';
 
 import express, {Express} from 'express';
+import cors from 'cors';
 const app: Express = express()
 
+app.use(cors());
+
 app.use(express.json());
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8100);
 
 connection.connect((err) => {
     if(err) throw err;
     console.log('Connection established');
-    // TODO EL CODIGO AQUI
 
     app.listen(app.get('port'), () => {
-        console.log('Server on port', 3000);
+        console.log('Server on port', app.get('port'));
     });
 });
 
